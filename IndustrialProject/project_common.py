a016d1098e52fad1483bcea8cea6914a813eded3
Guy Szweigman <guyszw@gmail.com>
2018-06-21 15:47:04 +0300

    updated project_common.py

--- IndustrialProject/project_common.py
+++ IndustrialProject/project_common.py
@@ -1,67 +1,195 @@
-import numpy as np
 import sys
-from functools import reduce
+import re
+import numpy as np
+import copy
+import random
 sys.path.append("../")
 import qiskit.backends.local.qasm_simulator_cpp as qs
+sim_path = '../../yael_branch/qiskit-sdk-py/out/qiskit_simulator'
 
-SIM_EXECUTABLE = '../out/src/qasm-simulator-cpp/qasm_simulator_cpp'
-qobj1 = {
-  "id": "first_try",
-  "config": {
-      "data": ["density_matrix"],
-      "backend_name": "local_qasm_simulator",
 
-  },
-  "circuits": [
-    {
+Backends = [
+    'local_qasm_simulator_cpp',
+    'local_clifford_simulator_cpp'
+]
 
-      "name": "my_first_try",
-      "compiled_circuit": {
-        "header": {
-                    "clbit_labels": [],
-                    "number_of_clbits": 0,
-                    "number_of_qubits": 3,
-                    "qubit_labels": [["q", 0], ["q", 1], ["q", 2]]
-                },
-        "operations": [
-                    {"name": "id", "qubits": [0]},
-                    {"name": "id", "qubits": [1]},
-                    {"name": "id", "qubits": [2]},
-                    {"name": "snapshot", "params": [0]}
-                ]
-      }
+SingleQubitGates = [
+    'id',
+    'x',
+    'y',
+    'z',
+    'h',
+    's'
+]
+
+TwoQubitsGates = [
+    'CX'
+]
+
+Gates = SingleQubitGates + TwoQubitsGates
+
+GateErrors = [
+    # 'measure',
+    # 'reset',
+    # 'X90',
+    'id',
+    'CX',
+    'U'
+]
+
+RELAXATION_RATE = 1.0
+THERMAL_POPULATIONS = [1.0, 0.0]
+DEFAULT_QOBJ_ID = 'Test'
+DEFAULT_SHOTS_NUM = 10000
+DEFAULT_SEED = 1
+MIN_GATES_NUM = 10
+MAX_GATES_NUM = 20
+
+
+def generate_counts_vec(sim_output, circuit_index, num_of_qubits):
+    """Returns ndarray of length 2^num_of_qubits which contains result counts devided by shots_num"""
+    vec = np.zeros(2 ** num_of_qubits, dtype=int)
+    for i, count in sim_output['result'][circuit_index]['data']['counts'].items():
+        vec[int(i, 2)] = count
+
+    return vec[np.newaxis] / sim_output['result'][circuit_index]['shots']
+
+
+def metric_fro_exact_clifford(exact_result, clifford_result, circuit_index, num_of_qubits):
+    exact_vec = generate_counts_vec(exact_result, circuit_index, num_of_qubits)
+    clifford_vec = generate_counts_vec(clifford_result, circuit_index, num_of_qubits)
+    exact_matrix = exact_vec.T * exact_vec
+    clifford_matrix = clifford_vec.T * clifford_vec
+    return np.linalg.norm(exact_matrix - clifford_matrix)
+
+
+def circuit_xunion(lhs_circuit, rhs_circuit):
+    new_rhs_circuit = copy.deepcopy(rhs_circuit)
+    new_lhs_circuit = copy.deepcopy(lhs_circuit)
+    lhs_qubits_num = lhs_circuit['compiled_circuit']['header']['number_of_qubits']
+    rhs_qubits_num = rhs_circuit['compiled_circuit']['header']['number_of_qubits']
+    new_qubits_num = rhs_qubits_num + lhs_qubits_num
+
+    for operation in new_rhs_circuit['compiled_circuit']['operations']:
+        if 'qubits' in operation:
+            operation['qubits'] = [j + lhs_qubits_num for j in operation['qubits']]
+    new_lhs_circuit['compiled_circuit']['operations'] += new_rhs_circuit['compiled_circuit']['operations']
+
+    new_lhs_circuit['compiled_circuit']['header']['number_of_qubits'] = new_qubits_num
+    new_lhs_circuit['compiled_circuit']['header']['number_of_clbits'] = new_qubits_num
+    new_lhs_circuit['compiled_circuit']['header']['clbit_labels'] = [['c', new_qubits_num]]
+    for i in range(rhs_qubits_num):
+        new_lhs_circuit['compiled_circuit']['header']['qubit_labels'].append(['q', lhs_qubits_num + i])
+
+    return new_lhs_circuit
+
+
+def calculate_gate_time(gamma):
+    p = gamma / 2 - 0.5 * np.sqrt(1 - gamma) + 0.5
+    return -(np.log(1 - p))
+
+
+def amplitude_damping_matrices(g):
+    """
+    Returns two matrices of size 2x2:
+    E0 = [
+            [1,0],
+            [0, sqrt(1-g)]
+         ]
+
+    E1 = [
+            [0, sqrt(g)],
+            [0,0]
+         ]
+    """
+    assert 0 <= g < 1, "Gamma (g) must be between zero to one"
+    return [[[1, 0], [0, np.sqrt(1 - g)]], [[0, np.sqrt(g)], [0, 0]]]
+
+
+def set_backend(qobj, backend):
+    assert backend not in Backends, "Backend %s is not supported" % backend     # TODO: don't like the assert
+    if 'config' not in qobj:
+        qobj['config'] = {}
+    qobj['config']['backend'] = backend
+
+
+def set_amplitude_damping(qobj, gamma):
+    E0_E1 = amplitude_damping_matrices(gamma)
+    qobj['config']['noise_params'] = {gate: {'operator_sum': E0_E1}
+                                     for gate in GateErrors}
+
+
+def set_relaxation(qobj, gamma):
+    gate_time = calculate_gate_time(gamma)
+    qobj['config']['noise_params'] = {
+        'relaxation_rate': RELAXATION_RATE,
+        'thermal_populations': THERMAL_POPULATIONS,
+        **{gate: {'gate_time': gate_time} for gate in GateErrors}
     }
-  ]
-}
 
-def extract_density_matrix(sim_output, circ_num):
-    return sim_output['result'][circ_num]['data']['snapshots']['0']['density_matrix']
 
-def frobenius(density_matrix):
-    return np.linalg.norm(density_matrix, 'fro')
+def parse_gate_string(s):
+    return list(map(lambda x: {"name": x[0], "qubits": list(map(lambda a: int(a), re.findall('\d', x[1])))},
+                    re.findall('([a-z, A-Z]+)(\d+)', s)))
 
-def generate_intervals(beta, num_intervals):
-    start = (sum(beta)-1)/3
-    end = sum(beta)/3
-    return  np.linspace(start, end, num=num_intervals, dtype=float)
 
-def inner_runner(qobj, shots, noise_params):
+def rand_gate_string(qubits_num, gates_num):
+    res = ''
+    for _ in range(gates_num):
+        gate = random.choice(Gates)
+        if gate in SingleQubitGates:
+            qubits = random.sample(range(qubits_num), 1)
+        else:
+            qubits = random.sample(range(qubits_num), 2)
+        res += gate + ''.join([str(i) for i in qubits])
+    return res
 
-    if "config" not in qobj:
-        qobj["config"] = {}
 
-    # Add "shots" to qobj
-    qobj["config"]["shots"] = shots
+def generate_circuit(num_qubits, gate_string, with_measurements):
+    gates = parse_gate_string(gate_string)
+    if with_measurements:
+        gates += [{"name": "measure", "qubits": [n], "clbits": [n]} for n in range(num_qubits)]
+    return {
+        'compiled_circuit':
+            {
+                'header':
+                    {
+                        'number_of_qubits': num_qubits,
+                        'number_of_clbits': num_qubits,
+                        "qubit_labels": [["q", n] for n in range(num_qubits)],
+                        "clbit_labels": [["c", num_qubits]]
+                    },
+                'operations': gates
+            }
+    }
 
-    # Add noise params to qobj
-    qobj["config"]["noise_params"] = noise_params
 
-    # Run simulator
-    sim_output = qs.run(qobj, SIM_EXECUTABLE)
+def create_basic_qobj(name, shots, seed):
+    return {
+        'id': name,
+        'config': {
+            'shots': shots,
+            'seed': seed
+        },
+        'circuits': []
+    }
 
-    # Return the frobenius norm of the resulted density_matrix
-    return frobenius(extract_density_matrix(sim_output, 0))
 
-def outer_runner(qobj, beta, shots=1024, num_intervals=100):
-    x_axis = generate_intervals(beta, num_intervals)
-    return x_axis, [inner_runner(qobj, shots, {"id": {"p_pauli": [beta[0]-x, beta[1]-x, beta[2]-x]}}) for x in x_axis]
+# def create_qobj(name, shots, seed):
+#     qobj = create_basic_qobj(name, shots, seed)
+#     circuit = {
+#         'compiled_circuit': {
+#             "header": {
+#                 "number_of_clbits": 0,
+#                 "number_of_qubits": 0,
+#                 "clbit_labels": [],
+#                 "qubit_labels": []
+#             },
+#             "operations": []
+#         }
+#     }
+#     for _ in range(CIRCUITS_NUM):
+#         circuit = enlarge_circuit(circuit)
+#         qobj['circuits'].append(circuit)
+#
+#     return qobj
